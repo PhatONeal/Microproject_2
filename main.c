5536f01eb43adae934e61a86f261a7fa04415ef0
Edward <edwardtabango@unicauca.edu.co>
2026-05-04 01:06:04 -0500

    Commit 5 - conversion a unidades fisicas y despliegue OLED
    -Se convierten los datos crudos del ADC a °C y porcentajes
    matemáticamente.
    -Se usan arreglos char buffer[] y sprintf para mostrar los datos en la
    OLED.

--- main.c
+++ main.c
@@ -8,29 +8,32 @@
 
 void main(void) {
     unsigned int adc_temp, adc_gas, adc_luz;
+    unsigned int temp_c, gas_p, luz_p;
+    char buffer[16];
 
-    OSCCON = 0x72; 
-    while (!OSCCONbits.IOFS);
+    OSCCON = 0x72; while (!OSCCONbits.IOFS);
+    ADCON1 = 0x0C; TRISA = 0x07; LATA = 0x00;
+    TRISBbits.TRISB0 = 1; TRISBbits.TRISB1 = 1; 
 
-    ADCON1 = 0x0C; 
-    TRISA = 0x07; 
-    LATA = 0x00;
-
-    // FIX: Pines I2C como entradas para el PIC18F4550
-    TRISBbits.TRISB0 = 1; 
-    TRISBbits.TRISB1 = 1; 
-
-    ADC_Init();
-    I2C_Init();
-    OLED_Init(); 
-
-    OLED_Clear();
-    OLED_String(1, 10, "PANTALLA OK");
+    ADC_Init(); I2C_Init(); OLED_Init(); OLED_Clear();
 
     while (1) {
         adc_temp = ADC_Leer(0);
         adc_gas = ADC_Leer(1);
         adc_luz = ADC_Leer(2);
-        __delay_ms(100);
+
+        // Conversiones matemáticas básicas
+        temp_c = (unsigned int)((adc_temp * 506UL) / 1023);
+        gas_p = (unsigned int)((adc_gas * 100UL) / 1023);
+        luz_p = (unsigned int)((adc_luz * 100UL) / 1023);
+
+        sprintf(buffer, "Temp: %u C  ", temp_c);
+        OLED_String(0, 0, buffer);
+        sprintf(buffer, "Gas: %u %%  ", gas_p);
+        OLED_String(2, 0, buffer);
+        sprintf(buffer, "Luz: %u %%  ", luz_p);
+        OLED_String(4, 0, buffer);
+        
+        __delay_ms(200);
     }
 }
